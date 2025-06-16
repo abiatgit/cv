da7c080fda522d64f26d5e6505e550ddfb809711
Abi George <contactabisgeorge@gmail.com>
2025-06-16 12:22:27 +0100

    text change

--- components/project/Project.tsx
+++ components/project/Project.tsx
@@ -11,7 +11,7 @@ const Project = () => {
           <div className="w-px h-3 bg-gray-300" />
           <p className="text-sm font-bold text-gray-600 dark:text-white">
             {" "}
-            CareHome Management App
+            CareHome Management SaaS
           </p>
           <Badge  className="bg-blue-100 text-blue-600 hover:bg-blue-300 hover:text-blue-900 px-2 py-0.5 text-xs rounded-md">
             <a href="https://carematters.vercel.app/" target="_blank" rel="noopener noreferrer">
@@ -27,12 +27,12 @@ const Project = () => {
       </div>
       <div className="my-2">
         <div className="flex gap-2 items-center">
-          <p className="text-sm font-bold text-gray-600 dark:text-white">taskify</p>
+          <p className="text-sm font-bold text-gray-600 dark:text-white">Taskify</p>
 
           <div className="w-px h-3 bg-gray-300"/>
           <p className="text-sm font-bold text-gray-600 dark:text-white">
             {" "}
-            Trello-like Project Management app
+            Trello-like Project Management App
           </p>
           <Badge  className="bg-emerald-100 text-emerald-600 hover:bg-emerald-300 hover:text-emerald-900 px-2 py-0.5 text-xs rounded-md">
             <a
@@ -50,7 +50,7 @@ const Project = () => {
       </div>
       <div className="my-2">
         <div className="flex gap-2 items-center">
-          <p className="text-sm font-bold text-gray-600 dark:text-white">get ƪooped</p>
+          <p className="text-sm font-bold text-gray-600 dark:text-white">Get ƪooped</p>
 
           <div className="w-px h-3 bg-gray-300" />
           <p className="text-sm font-bold text-gray-600 dark:text-white">
